0dd0602d054696bba63a102c3ccf0d442f52acdd
dakotasternadel <kotersternadel@gmail.com>
2025-08-24 21:59:05 -0500

    scadd

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseButton = editProfileModal.querySelector(
+  ".modal__close-button"
+);
+const newPostButton = document.querySelector(".profile__post-button");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+
+editProfileButton.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-open");
+});
+
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-open");
+});
+
+newPostButton.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-open");
+});
+
+newPostCloseButton.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-open");
+});
+
+console.log(hello);
