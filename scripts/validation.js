f51993839a90a5edb42f1d72eeed81301b450f7d
dakotasternadel <kotersternadel@gmail.com>
2025-12-23 12:23:58 -0600

    javascript errors implemented

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,13 +1,34 @@
+const showInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgId = formEl.querySelector(`#${inputEl.id}-error`);
+  errorMsgId.textContent = errorMsg;
+  inputEl.classList.add(".modal__input_type_error");
+};
+
+const hideInputError = (formEl, inputEl) => {
+  const errorMsgId = formEl.querySelector(`#${inputEl.id}-error`);
+  errorMsgId.textContent = "";
+  inputEl.classList.remove("modal__input_type_error");
+};
+
+const checkInputValidity = (formEl, inputEl) => {
+  if (!inputEl.validity.valid) {
+    console.log(inputEl.validationMessage);
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  } else {
+    hideInputError(formEl, inputEl);
+  }
+};
+
 const setEventListeners = (formEl) => {
-  const inputList = Array.from(formEl.querySelectorAll(".form__input"));
-  const buttonElement = formEl.querySelector(".form__submit");
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit-buttom");
 
-  toggleButtonState(inputList, buttonElement);
+  //toggleButtonState(inputList, buttonElement);
 
-  inputList.forEach((inputEl) => {
-    inputEl.addEventListenter("input", function () {
-      checkInputValidity(formEl, inputEl);
-      toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEl, inputElement);
+      // toggleButtonState(inputList, buttonElement);
     });
   });
 };
